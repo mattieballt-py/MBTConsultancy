e942b9478e5f6e447ccddcb5b63672a10008edea
Mattie <Mbt.jghs@gmail.com>
2025-06-13 13:06:51 +0200

    Fix asset paths to use relative URLs

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MBTConsultancy.com.c/',
+  base: './',
   build: {
     outDir: 'docs',
     emptyOutDir: true,
